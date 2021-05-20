380b255ce2c88215bf732109ed7bcb988fdfcc7d
caglar7 <mcaglarcnkr@gmail.com>
2021-05-20 20:55:25 +0300

    com port dropdown is done

---
+++ RocketMonitoring/Assets/Scripts/EntryManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EntryManager : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Dropdown dropDown_Ports;
+
+}
